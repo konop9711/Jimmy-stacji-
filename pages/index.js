5cf88406050f641c11f4ecc23627bc3cbec7c9b6
konop9711 <157829096+konop9711@users.noreply.github.com>
2024-12-16 20:12:30 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to staking app!</h1>
+        <h1 className={styles.title}>Welcome to Jimmy staking!</h1>
 
         <p className={styles.description}>
           Stake certain amount and get reward tokens back!
